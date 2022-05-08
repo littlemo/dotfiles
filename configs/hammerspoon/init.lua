571846c3263be2f9a0d164ee8851d9f7a8ed5a77
Moore.Huang <moore@moorehy.com>
2022-05-08 20:20:41 +0800

    [hammerspoon]Install: 添加插件，并完善按键映射

---
+++ configs/hammerspoon/init.lua
@@ -0,0 +1,29 @@
+hs.loadSpoon("SpoonInstall")
+local hyperKey = {'shift', 'alt', 'ctrl', 'cmd'}
+
+-- 快速启动应用
+spoon.SpoonInstall:andUse("AppLauncher", {
+  config = {
+    modifiers = hyperKey,
+  },
+  hotkeys = {
+    o = "Google Chrome",
+    t = "Alacritty",
+    l = "Logseq",
+    m = "Microsoft Outlook",
+    w = "企业微信",
+    c = "WeChat",
+    d = "TickTick",
+  },
+})
+
+-- 跨屏幕移动窗口
+spoon.SpoonInstall:andUse("WindowScreenLeftAndRight", {
+  config = {
+    animationDuration = 0,
+  },
+  hotkeys = {
+    screen_left = { hyperKey, "Left" },
+    screen_right= { hyperKey, "Right" },
+  },
+})
